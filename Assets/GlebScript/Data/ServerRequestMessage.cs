2560b4d8c5e33bcf86fbcfb852446b83712251d8
Hlib Bulakh <g.bulakh@come2play.com>
2019-05-24 14:31:06 +0300

    server request

--- Assets/GlebScript/Data/ServerRequestMessage.cs
+++ Assets/GlebScript/Data/ServerRequestMessage.cs
@@ -5,6 +5,28 @@ using UnityEngine;
 public class ServerRequestMessage
 {
     [SerializeField]
+    public int messageType;
+
+    public int MessageType
+    {
+        get { return messageType; }
+        set { messageType = value; }
+    }
+
+    [SerializeField]
+    public string data;
+
+    public string Data
+    {
+        get { return data; }
+        set { data = value; }
+    }
+}
+
+[Serializable]
+public class ServerMethodRequestMessage
+{
+    [SerializeField]
     public string methodName;
 
     public string MethodName
